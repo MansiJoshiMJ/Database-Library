d0ac4d735a7769b620e0a4ae6f9dfafc46bf0251
Mansi Joshi <mansi.joshi@otfcoder.com>
2019-11-07 13:42:12 +0530

    Third Commit

--- sqlitehelperlibrary/src/main/java/com/otfcoder/sqlitehelperlibrary/Table.java
+++ sqlitehelperlibrary/src/main/java/com/otfcoder/sqlitehelperlibrary/Table.java
@@ -6,8 +6,8 @@ public class Table {
 
     String tableName = "";
     HashMap<String, String> fieldList = new HashMap<>();
-    String whereClause = "";
-    String[] whereArgs = new String[] {};
+    public String whereClause = "";
+    public String[] whereArgs = new String[] {};
 
     public Table(String tableName, HashMap<String, String> fieldList) {
         this.tableName = tableName;
